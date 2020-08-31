2040d3110b2acc6e256ede1b06810dc22283d97d
3PIV <pprovins@vols.utk.edu>
2020-08-31 13:05:58 -0400

    Added analytics

--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
 import App from "./App.vue";
+import VueAnalytics from "vue-analytics";
 import router from "./router";
 import store from "./store";
 
@@ -9,6 +10,11 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 Vue.config.productionTip = false;
+
+Vue.use(VueAnalytics, {
+  id: 'UA-176823295-1',
+  disabled: false,
+});
 
 new Vue({
   router,
